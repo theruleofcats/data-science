532d7054b5f49c4dd8ca5a7f39ad90686e57bd61
Anoushka <winnieofcatar@gmail.com>
2025-08-14 11:05:44 +0000

    custom modules from the data science bootcamp

---
+++ data-science-bootcamp/week-0/custom-modules/functions.py
@@ -0,0 +1,7 @@
+def calculate_triangle_area(base, height):
+    area = 0.5 * base * height
+    return area
+
+def calculate_square_area(length):
+    area = length * length
+    return area
---
+++ data-science-bootcamp/week-0/custom-modules/program.py
@@ -0,0 +1,7 @@
+import functions
+
+area = functions.calculate_square_area(5)
+print(area)
+
+area = functions.calculate_triangle_area(5, 10)
+print(area)
